5394937c5826045171667d5bb2ab8730d5102980
RAHMAN PASHA <mythicalatom2020@gmail.com>
2025-09-06 09:01:11 +0000

    Initial prototype

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Header } from '@/components/header';
+import { DescriptionGenerator } from './description-generator';
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="flex flex-col min-h-screen bg-background">
+      <Header />
+      <main className="flex-1 flex flex-col items-center justify-center p-4 sm:p-6 md:p-8">
+        <DescriptionGenerator />
+      </main>
+    </div>
+  );
 }
